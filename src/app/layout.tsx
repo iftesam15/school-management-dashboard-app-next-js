ca2e6371289eb0e7e3b5e9f1ca314669ecb08f4c
iftesam15 <143783957+iftesam15@users.noreply.github.com>
2024-11-13 15:42:10 +0600

    fixed landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "IAB School Management Dashboard",
+  title: "IAB Dev School Management Dashboard",
   description: "Next.js School Management System",
 };
 
